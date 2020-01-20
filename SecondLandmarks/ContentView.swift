d4713680741fa69ae9af65957ed3e268ba4a80b7
Lohot Bintangur Siboro <bintangursiboro@gmail.com>
2020-01-20 08:21:07 +0700

    Initial Commit

---
+++ SecondLandmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SecondLandmarks
+//
+//  Created by LPT 531 on 20/01/20.
+//  Copyright © 2020 IJNIC Lohot. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SecondLandmarksTests/SecondLandmarksTests.swift
@@ -0,0 +1,34 @@
+//
+//  SecondLandmarksTests.swift
+//  SecondLandmarksTests
+//
+//  Created by LPT 531 on 20/01/20.
+//  Copyright © 2020 IJNIC Lohot. All rights reserved.
+//
+
+import XCTest
+@testable import SecondLandmarks
+
+class SecondLandmarksTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
